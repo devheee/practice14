40f6705bad4203782a2561e9c6ffd2f4331d5df1
SOHEE <soheee94@gmail.com>
2023-02-28 17:39:25 +0900

    230228

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,7 +1,25 @@
 const Footer = () => {
     return (
         <footer>
-            &copy; hee.
+            <ul >
+                <p>COMPANY</p>
+                <li className="f_first">Company : (주)클리오 | CEO : 한현옥</li>
+                <li className="f_first">Business License : 2118622189 | Tel : 080-080-1510</li>
+                <li className="f_first">Address : 04778 서울특별시 성동구 왕십리로 66 (성수동1가) 클리오</li>
+            </ul>
+            <ul>
+                <p>FOLLOW</p>
+                <li class="f_bold">Instagram</li>
+                <li class="f_bold">Facebook</li>
+                <li class="f_bold">Youtube</li>
+            </ul>
+            <ul>
+                <p>FAMLIY SITE</p>
+                <li class="f_bold">Club clio</li>
+                <li class="f_bold">English</li>
+                <li class="f_bold">Chinese</li>
+                <span>&copy; 2021 goodal</span>
+            </ul>
         </footer>
     )
 }
--- src/component/Main.js
+++ src/component/Main.js
@@ -4,10 +4,16 @@ const Main = () => {
     return (
         <>
             <section className="Mainimg">
-                <Link to="/01"><img src={process.env.PUBLIC_URL + '/img/main01.jpg'} alt="" /></Link>
+                {/* <Link to="/01"><img src={process.env.PUBLIC_URL + '/img/main01.jpg'} alt="" /></Link> */}
             </section>
             <section className="MainPhrases">
-
+                <h2>Good all, goodal</h2>
+                <p>"피부에 전하는 순한 영향력, 우리 모두에게 좋은 것으로"</p>
+                <Link><span>Brand story</span></Link>
+            </section>
+            <section className="MainVideo">
+                <iframe src="https://player.vimeo.com/video/749442044?h=8fc3f4fb0f&background=1&autoplay=1&loop=1&title=0&byline=0&portrait=0&controls=0&muted=1" frameborder="">
+                </iframe>
             </section>
         </>
     )
